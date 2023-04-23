c0716397d908342a2848601c22a352f3d120b0d5
Hsn abs <abbasihsn@Hsns-MacBook-Air.local>
2023-04-23 14:42:22 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SpyfallGame from "./SpyfallGame";
+import LoginPage from "./LoginPage";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loggedIn ? (
+        <SpyfallGame />
+      ) : (
+        <LoginPage onLogin={handleLogin} />
+      )}
     </div>
   );
 }
---
+++ src/LoginPage.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './LoginPage.css';
+
+function LoginPage({ onLogin }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === 'username' && password === 'password') {
+        onLogin();
+    } else {
+      setLoginError(true);
+    }
+  };
+
+  return (
+    <div className="container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="login-input"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="login-input"
+        />
+        {loginError && (
+          <p className="login-error">
+            Incorrect username or password. Please try again.
+          </p>
+        )}
+        <button type="submit" className="login-button">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
